b0005e7fe7af026cec9427e286b1a60c065bcd83
Charlie Bevis <charliebevis@gmail.com>
2020-02-18 17:35:00 -0600

    here's a goofy way to parse a decimal number

--- src/AltParsing.hs
+++ src/AltParsing.hs
@@ -4,19 +4,28 @@ import Text.Trifecta
 import Text.Fractions
 
 type DecimalOrFraction =
-  Either Rational Integer
+  Either Rational Float
 
 frac = "1/2"
-dec = "123"
+dec = "1.23"
 
 parseNos :: Parser DecimalOrFraction
 parseNos =
   (try $ Left <$> virtuousFraction)
   <|> (Right <$> parseDecimal)
 
-parseDecimal :: Parser Integer
-parseDecimal = decimal
-
+parseDecimal :: Parser Float
+parseDecimal = do
+  a <- (token decimal)
+  char '.'
+  b <- (token decimal)
+  -- how do we divide this by ten the right number of times?
+  return $ fromIntegral a + toDecimal (fromIntegral b)
+  
+toDecimal :: Float -> Float
+toDecimal i =
+  if i < 1 then i
+                else toDecimal (i/10)
 main' = do
   let p f i = parseString f mempty i
   print $ p (parseNos) frac
